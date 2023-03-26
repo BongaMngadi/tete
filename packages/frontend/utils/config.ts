d431fe0b7e53550c788be6c6806b7150db0a36ae
BongaConnection <dlokovu100@gmail.com>
2023-03-27 01:06:55 +0200

    first commit

--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,7 +1,7 @@
-const API_PRODUCTION_URL = 'http://localhost:4000';
+const API_PRODUCTION_URL = 'https://api.getorca.org';
 const API_DEV_URL = 'http://localhost:4000';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
-  GOOGLE_ANALYTICS_ID: 'G-MD706N8TZV',
+  GOOGLE_ANALYTICS_ID: 'G-9K5Q4L9EQE',
 };
